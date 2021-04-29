3c652865712788e4d8ea93d29cbacff5f49da44f
chienleow <chienleow1@gmail.com>
2021-04-28 22:45:56 -0400

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://hn.algolia.com/api")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result.items
+        })
+      }
+    )
+  }
 
 
 }
